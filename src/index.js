b8b379a378d21509a7f633314cae1a32deb4327b
juliastetskaya <juliastetskaya@gmail.com>
2018-02-20 10:12:50 +0300

    Step 1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,5 @@ export default () => {
     .description('Compares two configuration files and shows a difference.')
     .arguments('<firstConfig> <secondConfig>')
     .option('-f, --format [type]', 'Output format')
-    .parse(process.argv);
-
-  if (!program.args.length) program.help();
+    .action(program.help());
 };
